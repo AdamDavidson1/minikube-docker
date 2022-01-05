c575d219aad6155325249f51481e30347b968b8d
Adam Davidson <adavidson@coredigital.com>
2022-01-04 19:31:19 -0800

    feat: addressed auto start cluster and adjusted uninstaller

--- install.py
+++ install.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import subprocess
+import re
 
 def failure():
     print("You need local admin or contact helpdesk.")
@@ -15,31 +17,38 @@ def dependencies():
         failure()
 
 def setup_profile():
+    if os.environ.get("PATH").find("/opt/local/bin") > -1:
+        return True
     print("Modifying PATH")
-    if os.path.isfile("~/.profile"):
-        os.system("echo 'export PATH=\"/opt/local/bin:$PATH\"' >> ~/.profile")
-        os.system("source ~/.profile")
+    user = subprocess.check_output(['id', '-un']).decode('UTF-8')[0:-1]
+    if os.path.isfile(f"/User/{user}/.profile"):
+        os.system(f"echo 'export PATH=\"/opt/local/bin:$PATH\"' >> /User/{user}/.profile")
+        os.system(f"source /User/{user}/.profile")
         return True
-    elif os.path.isfile("~/.bash_profile"):
-        os.system("echo 'export PATH=\"/opt/local/bin:$PATH\"' >> ~/.bash_profile")
-        os.system("source ~/.bash_profile")
+    elif os.path.isfile(f"/User/{user}/.bash_profile"):
+        os.system(f"echo 'export PATH=\"/opt/local/bin:$PATH\"' >> /User/{user}/.bash_profile")
+        os.system(f"source /User/{user}/.bash_profile")
         return True
-    elif os.path.isfile("~/.bashrc"):
-        os.system("echo 'export PATH=\"/opt/local/bin:$PATH\"' >> ~/.bashrc")
-        os.system("source ~/.bashrc")
+    elif os.path.isfile(f"/User/{user}/.bashrc"):
+        os.system(f"echo 'export PATH=\"/opt/local/bin:$PATH\"' >> /User/{user}/.bashrc")
+        os.system(f"source /User/{user}/.bashrc")
         return True
-    elif os.path.isfile("~/.zshrc"):
-        os.system("echo 'export PATH=\"/opt/local/bin:$PATH\"' >> ~/.zshrc")
-        os.system("source ~/.zshrc")
+    elif os.path.isfile(f"/User/{user}/.zshrc"):
+        os.system(f"echo 'export PATH=\"/opt/local/bin:$PATH\"' >> /User/{user}/.zshrc")
+        os.system(f"source /User/{user}/.zshrc")
         return True
     return False
 
+user = subprocess.check_output(['id', '-un']).decode('UTF-8')[0:-1]
+
 dependencies()
-if os.path.isdir("/Applications/Docker.app"):
+if os.path.isdir("/Applications/Docker.app") and input("Do you have Docker Desktop Installed? N/y ").lower() == 'y':
     os.system("sudo /Applications/Docker.app/Contents/MacOS/Docker --uninstall")
 
 if os.system("brew install minikube docker docker-compose") > 1:
     failure()
+os.system(f"mkdir -p /Users/{user}/bin")
+os.system(f"cp start-minikube.sh /Users/{user}/bin/")
 
 if os.system("sudo mkdir -p /opt/local/bin") > 1:
     failure()
@@ -47,20 +56,20 @@ if os.system("sudo cp hyperkit vpnkit /opt/local/bin/") > 1:
     failure()
 if os.system("sudo chmod 755 /opt/local/bin/hyperkit /opt/local/bin/vpnkit") > 1:
     failure()
-if os.system("sudo cp vpnkit.plist /Library/LaunchDaemons/com.coredigital.vpnkit.plist") > 1:
+if os.system("sudo cp com.coredigital.vpnkit.plist /Library/LaunchDaemons/") > 1:
     failure()
-if os.system("sudo launchctl load -w /Library/LaunchDaemons/com.coredigital.vpnkit") > 1:
+if os.system("launchctl load -w /Library/LaunchDaemons/com.coredigital.vpnkit.plist") > 1:
     failure()
 
 if setup_profile() != True:
     print("PATH failed to setup, using inline modification")
     os.system("export PATH=\"/opt/local/bin:$PATH\"")
 
-if os.system("minikube start --driver=hyperkit --hyperkit-vpnkit-sock=/var/run/vpnkit.socket") > 1:
+if os.system("minikube status") == 0:
+    print("Existing cluster was found!")
+    print("Please run: minikube stop && minikube delete and try again.")
+    sys.exit(2)
+if os.system("~/bin/start-minikube.sh") > 1:
     failure()
 os.system("minikube kubectl -- get pods -A")
-os.system("minikube stop")
-if os.system("sudo cp minikube.plist /Library/LaunchDaemons/com.coredigital.minikubemachine.plist") > 1:
-    failure()
-if os.system("sudo launchctl load -w /Library/LaunchDaemons/com.coredigital.minikubemachine") > 1:
-    failure()
+os.system("osascript -e 'tell application \"System Events\" to make login item at end with properties {name:\"Minikube Docker\", path:\"/User/" + user + "/bin/start-minikube.sh\", hidden:true}'")
--- uninstall.py
+++ uninstall.py
@@ -10,14 +10,10 @@ print("Requiring sudo. Please enter your password to continue")
 
 os.system("minikube stop")
 os.system("minikube delete")
-if os.system("sudo launchctl unload -w /Library/LaunchDaemons/com.coredigital.minikubemachine") > 1:
+if os.system("launchctl remove com.coredigital.vpnkit") > 1:
     failure()
-if os.system("sudo launchctl unload -w /Library/LaunchDaemons/com.coredigital.vpnkit") > 1:
-    failure()
-if os.system("sudo rm /opt/local/bin/hyperkit /opt/local/bin/vpnkit") > 1:
-    failure()
-if os.system("sudo rm /Library/LaunchDaemons/com.coredigital.vpnkit /Library/LaunchDaemons/com.coredigital.minikubemachine") > 1:
-    failure()
+os.system("sudo rm /opt/local/bin/hyperkit /opt/local/bin/vpnkit")
+os.system("sudo rm /Library/LaunchDaemons/com.coredigital.vpnkit.plist")
 remove_docker = input("Do you want to remove minikube, docker, and docker-compose? N/y ")
 if remove_docker.lower() == "y":
     os.system("brew uninstall minikube docker docker-compose")
